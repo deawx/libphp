e937e1cafb4fcf0bb910c08715baee2dcde98bbf
John Habila <36119859+PHPMaster88@users.noreply.github.com>
2023-03-25 16:11:11 +0100

    Add files via upload

--- src/lib/timeClass.lib.php
+++ src/lib/timeClass.lib.php
@@ -7,7 +7,7 @@
      * For the full copyright and license information, please view the LICENSE
      * file that was distributed with this source code.
      */
-    namespace PHPMaster\lib;
+    namespace PHPMaster88\lib;
 	class timeClass{
 		// PRIVATE PROPERTIES
         private $unix = 0;
--- src/lib/zipClass.lib.php
+++ src/lib/zipClass.lib.php
@@ -7,7 +7,7 @@
      * For the full copyright and license information, please view the LICENSE
      * file that was distributed with this source code.
 	 */
-    namespace PHPMaster\lib;
+    namespace PHPMaster88\lib;
 	class zipClass{
 		// PRIVATE PROPERTIES
         private $version = "2.1.5";
